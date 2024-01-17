24cc1a0c12f696d9ebd0f2bb85a28f219af26237
Irwin <irwin.soliman@gmail.com>
2024-01-17 14:47:07 +0100

    merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,41 @@
+import React from "react";
+import { useState } from "react";
+import Webcam from "react-webcam";
+
+const videoConstraints = {
+  width: 1280,
+  height: 720,
+  facingMode: "user",
+};
+
 function App() {
-  return <div className="app">coucou</div>;
+  const [photo, setphoto] = useState();
+
+  const webcamRef = React.useRef(null);
+
+  const capture = React.useCallback(() => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    console.log(imageSrc);
+    setphoto(imageSrc);
+  }, [webcamRef]);
+
+  return (
+    <>
+      <Webcam
+        audio={false}
+        height={720}
+        ref={webcamRef}
+        screenshotFormat="image/jpeg"
+        width={1280}
+        videoConstraints={videoConstraints}
+      />
+      <button onClick={capture}>Capture photo</button>
+
+      <div>
+        <img src={photo} alt="" />
+      </div>
+    </>
+  );
 }
 
 export default App;

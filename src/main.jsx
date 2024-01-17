36e0024e45ffed9cb21d32a6120b56c6b3b7dd47
Emmanuelle BONOLI <manu.bonoli@gmail.com>
2024-01-17 14:25:26 +0100

    creation router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,40 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.jsx";
+import Home from "./pages/Home.jsx";
+import Login from "./pages/Login.jsx";
+import Photomaton from "./pages/Photomaton.jsx";
+import Styliste from "./pages/Styliste.jsx";
 import "./styles/index.scss";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    id: "app",
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "/photomaton",
+        element: <Photomaton />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/stylisteprèsdechezvous",
+        element: <Styliste />,
+      },
+    ]
+  }])
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+ <RouterProvider router={router} />
   </React.StrictMode>
 );

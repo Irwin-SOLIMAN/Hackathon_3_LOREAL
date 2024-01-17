2d9cae592e70f36d5b2077667bff0cc99edd4a50
Emmanuelle BONOLI <manu.bonoli@gmail.com>
2024-01-17 14:28:50 +0100

    creation router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import { Outlet } from "react-router-dom";
+import Header from "./components/Header.jsx";
+
 function App() {
-  return <div className="app">coucou</div>;
+  return (
+    <div className="app">
+      <Header />
+      <Outlet />
+    </div>
+  );
 }
 
 export default App;

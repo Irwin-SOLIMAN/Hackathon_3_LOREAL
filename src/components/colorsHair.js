884008220f78562a7ce2a8a78790f297a4e7fe36
Emmanuelle BONOLI <manu.bonoli@gmail.com>
2024-01-17 16:51:10 +0100

    ajout photos couleurs cheveux

---
+++ src/components/colorsHair.js
@@ -0,0 +1,69 @@
+const colorsHair = [
+    {
+        id: 1,
+        image: "/images/colorsHair/color1.png",
+        color: "brown",
+    },
+    {
+        id: 2,
+        image: "/images/colorsHair/color2.png",
+        color: "brown",
+    },
+    {
+        id: 3,
+        image: "/images/colorsHair/color3.png",
+        color: "brown",
+    },
+    {
+        id: 4,
+        image: "/images/colorsHair/color4.png",
+        color: "darkBlond",
+    },
+    {
+        id: 5,
+        image: "/images/colorsHair/color5.png",
+        color: "darkBlond",
+    },
+    {
+        id: 6,
+        image: "/images/colorsHair/color6.png",
+        color: "blond",
+    },
+    {
+        id: 7,
+        image: "/images/colorsHair/color7.png",
+        color: "blond",
+    },
+    {
+        id: 8,
+        image: "/images/colorsHair/color8.png",
+        color: "DarkBlond",
+    },
+    {
+        id: 9,
+        image: "/images/colorsHair/color9.png",
+        color: "blond",
+    },
+    {
+        id: 10,
+        image: "/images/colorsHair/color10.png",
+        color: "blond",
+    },
+    {
+        id: 11,
+        image: "/images/colorsHair/color11.png",
+        color: "blond",
+    },
+    {
+        id: 12,
+        image: "/images/colorsHair/color16.png",
+        color: "red",
+    },
+    {
+        id: 17,
+        image: "/images/colorsHair/color17.png",
+        color: "gray",
+    }
+  ]
+
+  export default colorsHair;

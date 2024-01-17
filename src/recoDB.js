b5188bfcf0646e9b0f275784f9cfd41ed1efb773
Guillaume Lebeau <guillaume.lebeau44@hotmail.fr>
2024-01-17 16:11:39 +0100

    commit b4 pull

---
+++ src/recoDB.js
@@ -0,0 +1,53 @@
+const reco = [
+    {
+        id: 1,
+        name : "Préférence Blondes",
+        price: "12.20€",
+        categ: "blond",
+        img: "src/public/logo/teinture_blond1.png",
+    },
+    {
+        id: 2,
+        name : "Excellence Crème Blondes",
+        price: "11.70€",
+        categ: "blod",
+        img: "src/public/logo/teinture_blond2.png",
+    },
+    {
+        id: 3,
+        name : "Casting Crème Gloss Blondes",
+        price: "10.90€",
+        categ: "blond",
+        img: "src/public/logo/teinture_blond3.png",
+    },
+    {
+        id: 4,
+        name : "Préférence Châtains",
+        price: "11.90€",
+        categ: "chatain",
+        img: "src/public/logo/teinture_chatain1.png",
+    },
+    {
+        id: 5,
+        name : "Excellence Crème Châtain",
+        price: "11.70€",
+        categ: "chatain",
+        img: "src/public/logo/teinture_chatain2.png",
+    },
+    {
+        id: 6,
+        name : "Casting Crème Gloss Châtain",
+        price: "10.90€",
+        categ: "chatain",
+        img: "src/public/logo/teinture_chatain3.png",
+    },
+    {
+        id: 7,
+        name : "ELSEVE COLOR-VIVE SHAMPOING VIOLET DÉJAUNISSEUR",
+        price: "4.14€",
+        categ: "soin",
+        img: "src/public/logo/soin_cheveux1.png",
+    },
+];
+
+export default reco;

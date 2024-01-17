c71b074e72c028eb89442ecbc0c718bf7270b782
Emmanuelle BONOLI <manu.bonoli@gmail.com>
2024-01-17 19:19:54 +0100

    avancée photomaton

---
+++ src/components/hairCutH.js
@@ -0,0 +1,16 @@
+const HairCutH =[{
+    id: 1,
+    image: "/images/hairCutH/hairH1.png",
+},
+{
+    id: 2,
+    image: "/images/hairCutH/hairH2.png",
+},
+{
+    id:3,
+    image: "/images/hairCutH/hairH3.png",
+},
+{
+    id: 4,
+    image: "/images/hairCutH/hairH4.png",
+}]
--- src/pages/Photomaton.jsx
+++ src/pages/Photomaton.jsx
@@ -1,27 +1,31 @@
 import MenuBurger from "../components/MenuBurger";
 import { useOutletContext } from "react-router-dom";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 const Photomaton = () => {
+  const [photo, setphoto] = useOutletContext();
+  const [openMenu, setOpenMenu] = useState(false);
+  const [chooseCut, setChooseCut] = useState("");
 
-const[photo, setphoto] = useOutletContext();
-const [openMenu, setOpenMenu] = useState(false);
-
-useEffect(() => {
-    console.log(photo);
-},[])
-
-function handleMenu(){
+  function handleMenu() {
     setOpenMenu(true);
-}
+  }
 
   return (
-  <div className="photomaton">
-          <div>
-        <img src={photo} alt="photo-capturer"/>
-      </div>
-    {openMenu === false ? <button className="buttonMenu" onClick={handleMenu}>{`<`}</button>: <MenuBurger openMenu={openMenu} setOpenMenu={setOpenMenu}/> }
-  </div>
-)};
+    <div className="photomaton">
+      <img className="photoUser" src={photo} alt="photo-capturer" />
+      {chooseCut && <img className={chooseCut.className} src={chooseCut.image} /> }
+      {openMenu === false ? (
+        <button className="buttonMenu" onClick={handleMenu}>{`<`}</button>
+      ) : (
+        <MenuBurger
+          setChooseCut={setChooseCut}
+          openMenu={openMenu}
+          setOpenMenu={setOpenMenu}
+        />
+      )}
+    </div>
+  );
+};
 
 export default Photomaton;

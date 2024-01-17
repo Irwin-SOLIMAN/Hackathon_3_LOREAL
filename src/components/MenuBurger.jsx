24f5dd914e9dbbc0e9f787737bd6eb83db19d7c2
Emmanuelle BONOLI <manu.bonoli@gmail.com>
2024-01-17 16:11:15 +0100

    page photo

--- src/components/MenuBurger.jsx
+++ src/components/MenuBurger.jsx
@@ -30,11 +30,11 @@ const MenuBurger = ({ openMenu, setOpenMenu }) => {
         <button className="buttonPhotomaton" onClick={handleCut}>
           Coupe
         </button>
-        {openCut ? <div className="chooseCut">blabla coupe</div> : ""}
+        {openCut ? <div className="chooseCut"><p className="switch">{`<`}</p>blabla coupe<p className="switch">{`>`}</p></div> : ""}
         <button className="buttonPhotomaton" onClick={handleColor}>
           Couleur
         </button>
-        {openColor ? <div className="chooseColor">blabla couleur</div> : ""}
+        {openColor ? <div className="chooseColor"><p className="switch">{`<`}</p>blabla couleur<p className="switch">{`>`}</p></div> : ""}
       </div>
       <button className="buttonPhotomaton">Valider</button>
       <button className="buttonClose" onClick={handleClose}>{`<`}</button>

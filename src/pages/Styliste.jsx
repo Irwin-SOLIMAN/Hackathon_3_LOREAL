d43ed1133c2e7c71adf02e539025db88a0f982ef
Irwin <irwin.soliman@gmail.com>
2024-01-18 11:47:42 +0100

    before merge

--- src/pages/Styliste.jsx
+++ src/pages/Styliste.jsx
@@ -1,132 +1,154 @@
-import React, { useEffect, useState } from 'react';
-import { useOutletContext } from 'react-router-dom';
-import reco from '../recoDB';
-
+import React, { useEffect, useState } from "react";
+import { useOutletContext } from "react-router-dom";
+import reco from "../recoDB";
+import { useScreenshot } from "use-react-screenshot";
 
 const Styliste = () => {
+  //   const [chooseColor, setChooseColor] = useOutletContext();
+  //   const [screenshot, setScreenshot] = useOutletContext();
+  const { chooseColor, screenshot } = useOutletContext();
+  const [stylistMap, setStylistMap] = useState(false);
+  const [cityValue, setCityValue] = useState("");
+  const [stylist1, setStylist1] = useState(false);
+  const [stylist2, setStylist2] = useState(false);
 
-    const [chooseColor, setChooseColor] = useOutletContext();
+  useEffect(() => {
+    console.log(chooseColor);
+  }, []);
 
-    const [stylistMap, setStylistMap] = useState(false);
-    const [cityValue, setCityValue] = useState('');
-    const [stylist1, setStylist1] = useState(false);
-    const [stylist2, setStylist2] = useState(false);
-
-    useEffect(() => {
-        console.log(chooseColor);
-    }, [])
-
-    const handleInputChange = (event) => {
-        const city = event.target.value.toLowerCase();
-        if(city === "nantes") {
-            setStylistMap(true);
-        } else {
-            setStylistMap(false);
-        }
-        setCityValue(city);
+  const handleInputChange = (event) => {
+    const city = event.target.value.toLowerCase();
+    if (city === "nantes") {
+      setStylistMap(true);
+    } else {
+      setStylistMap(false);
     }
+    setCityValue(city);
+  };
 
-    const handleClose = () => {
-        setStylistMap(false);
-        setCityValue('');
+  const handleClose = () => {
+    setStylistMap(false);
+    setCityValue("");
+  };
+
+  const handleStylist1 = () => {
+    setStylist1(true);
+    if (setStylist2 === true) {
+      setStylist2(false);
     }
-
-    const handleStylist1 = () => {
-        setStylist1(true)
-        if(setStylist2 === true) {
-            setStylist2(false)
-        }
-        console.log("styliste1")
+    console.log("styliste1");
+  };
+  const handleStylist2 = () => {
+    setStylist2(true);
+    if (setStylist1 === true) {
+      setStylist1(false);
     }
-    const handleStylist2 = () => {
-        setStylist2(true)
-        if(setStylist1 === true) {
-            setStylist1(false)
-        }
-        console.log("styliste2")
-    }
+    console.log("styliste2");
+  };
 
-    const closeCard1 = () => {
-        setStylist1(false)
-    }
+  const closeCard1 = () => {
+    setStylist1(false);
+  };
 
-    const closeCard2 = () => {
-        setStylist2(false)
-    }
+  const closeCard2 = () => {
+    setStylist2(false);
+  };
 
-    const recoFilteredByColor = reco.filter(product => product.categ === chooseColor || product.categ === "soin");
+  const recoFilteredByColor = reco.filter(
+    (product) => product.categ === chooseColor || product.categ === "soin"
+  );
 
-    return (
-        <div className='styliste'>
-            <div className='finderBlock'>
-                <p>
-                    Je trouve un styliste L'ORÉAL
-                </p>
-                <input type='text' placeholder='📍 Votre ville' onChange={handleInputChange} value={cityValue}>
-                </input>
-                {stylistMap && 
-                    <div className='map'>
-                        <div className='closingDiv' onClick={handleClose}>
-                            <p>Fermer la carte</p>
-                            <div className='closingButton'>✖️</div>
-                        </div>
-                        <img src='/images/maps1.png'/>
-                        <div className='pin1' onClick={handleStylist1}>
-                            <img src='/logo/location_4315546.png'/>
-                        </div>
-                        <div className='pin2' onClick={handleStylist2}>
-                            <img src='/logo/location_4315546.png'/>
-                        </div>
-                        {stylist1 &&
-                            <div className='stylistCard1'>
-                                <div className='closingButton' onClick={closeCard1}>✖️</div>
-                                <div className='infos'>
-                                    <div className='salon'>Innovatif</div>
-                                    <div className='adress'>4 Boulevard Dupont</div>
-                                    <div className='contact'>02.54.98.20.21</div>
-                                </div>
-                            </div>
-                        }
-                        {stylist2 &&
-                            <div className='stylistCard2'>
-                                <div className='closingButton' onClick={closeCard2}>✖️</div>
-                                <div className='infos'>
-                                    <div className='salon'>Coupe de Cœur</div>
-                                    <div className='adress'>35 Rue du Calvaire</div>
-                                    <div className='contact'>02.65.34.75.40</div>
-                                </div>
-                            </div>
-                        }
-                    </div>
-                }
+  return (
+    <div className="styliste">
+      <div>
+        <img
+          width={150}
+          style={{ marginTop: 20 }}
+          src={screenshot}
+          alt={"Screenshot"}
+        />
+      </div>
+      <div className="finderBlock">
+        <p>Je trouve un styliste L'ORÉAL</p>
+        <input
+          type="text"
+          placeholder="📍 Votre ville"
+          onChange={handleInputChange}
+          value={cityValue}
+        ></input>
+        {stylistMap && (
+          <div className="map">
+            <div className="closingDiv" onClick={handleClose}>
+              <p>Fermer la carte</p>
+              <div className="closingButton">✖️</div>
             </div>
-            <div className={stylistMap === true ? 'recommendedBlockHidden' : 'recommendedBlock'}>
-                <p>
-                    Je consulte les produits recommandés
-                </p>
-                <div className='productsMap'>
-                    {recoFilteredByColor.map((product) => {
-                        return (
-                            <div className='productCard' key={product.id}>
-                                <div className='imgContainer'>
-                                    <img src={product.img} />
-                                </div>
-                                <div className='productInfo'>
-                                    <div className='productName'>{product.name.toUpperCase()}</div>
-                                    <div className='productPrice'>{product.price} </div>    
-                                </div>
-                            </div>
-                            
-                        )
-                    })}
-                </div>
-                <div className={stylistMap === true ? 'unrollArrow' : 'unrollArrowHidden'} onClick={handleClose}>
-                    ⬇️
-                </div>
+            <img src="/images/maps1.png" />
+            <div className="pin1" onClick={handleStylist1}>
+              <img src="/logo/location_4315546.png" />
             </div>
-            <div/>
+            <div className="pin2" onClick={handleStylist2}>
+              <img src="/logo/location_4315546.png" />
+            </div>
+            {stylist1 && (
+              <div className="stylistCard1">
+                <div className="closingButton" onClick={closeCard1}>
+                  ✖️
+                </div>
+                <div className="infos">
+                  <div className="salon">Innovatif</div>
+                  <div className="adress">4 Boulevard Dupont</div>
+                  <div className="contact">02.54.98.20.21</div>
+                </div>
+              </div>
+            )}
+            {stylist2 && (
+              <div className="stylistCard2">
+                <div className="closingButton" onClick={closeCard2}>
+                  ✖️
+                </div>
+                <div className="infos">
+                  <div className="salon">Coupe de Cœur</div>
+                  <div className="adress">35 Rue du Calvaire</div>
+                  <div className="contact">02.65.34.75.40</div>
+                </div>
+              </div>
+            )}
+          </div>
+        )}
+      </div>
+      <div
+        className={
+          stylistMap === true ? "recommendedBlockHidden" : "recommendedBlock"
+        }
+      >
+        <p>Je consulte les produits recommandés</p>
+        <div className="productsMap">
+          {recoFilteredByColor.map((product) => {
+            return (
+              <div className="productCard" key={product.id}>
+                <div className="imgContainer">
+                  <img src={product.img} />
+                </div>
+                <div className="productInfo">
+                  <div className="productName">
+                    {product.name.toUpperCase()}
+                  </div>
+                  <div className="productPrice">{product.price} </div>
+                </div>
+              </div>
+            );
+          })}
         </div>
-    );
+        <div
+          className={stylistMap === true ? "unrollArrow" : "unrollArrowHidden"}
+          onClick={handleClose}
+        >
+          ⬇️
+        </div>
+      </div>
+      <div />
+    </div>
+  );
 };
 
 export default Styliste;
